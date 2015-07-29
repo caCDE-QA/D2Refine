fd9727ee11eb73f7624c3e7817af99efe2711fb4
Deepak Sharma <sharma.deepak2@mayo.edu>
2015-07-29 16:16:47 -0500

    setup complete

--- src/edu/mayo/d2refine/ADLExporterExtension/ADLExporter.java
+++ src/edu/mayo/d2refine/ADLExporterExtension/ADLExporter.java
@@ -1,27 +1,32 @@
 package edu.mayo.d2refine.ADLExporterExtension;
 import java.io.IOException;
-
 import java.io.Writer;
 import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.refine.browsing.Engine;
 import com.google.refine.model.Project;
 import com.google.refine.exporters.WriterExporter;
 
-import edu.mayo.samepage.adl.reader.*;
-
-
+import edu.mayo.samepage.utils.ADLUtils;
 
 public class ADLExporter implements WriterExporter
 {
+    final static Logger logger = LoggerFactory.getLogger("ADLExporter");
  
-    public String getContentType(){
-        return "application/x-unknown";
+    @Override
+    public String getContentType() 
+    {
+        return "text/plain";
     }
+    
     public void export(Project project, Properties options, Engine engine, Writer writer) throws IOException
     {
-           ADL2Reader reader = new ADL2Reader();
+           String reader = ADLUtils.getTestArchetypeText();
            System.out.println("This is tested");
+           writer.write(reader);
            return;
     }
 }
